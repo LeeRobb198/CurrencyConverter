f9b05cea9b09c608fa14fe110e0ae322662bb2d9
Lee Robbie <leerobb198@lees-mbp.home>
2020-02-07 22:37:03 +0000

    Divs added to separate content

--- currencyAPI.js
+++ currencyAPI.js
@@ -1,10 +1,23 @@
-// URL
+// Exchange rates against given currency ---------------------------------------
 
-var url = "https://api.exchangeratesapi.io/latest";
+$("#convertButton").click(function(){
+  console.log("Click registered");
 
-// Get JSON data ---------------------------------------------------------------
+  // Get chosen currencies from dropdown
+  var ccOne = document.getElementById("currencyOne");
+  var chosenCurrencyOne = ccOne.options[ccOne.selectedIndex].value;
 
-$.getJSON( url , function(data) {
-  console.log("Currency Data");
-  console.log(data);
+  console.log(chosenCurrencyOne);
+
+  var ccTwo = document.getElementById("currencyTwo");
+  var chosenCurrencyTwo = ccTwo.options[ccTwo.selectedIndex].value;
+
+  // URL---------------------------------------
+
+  var url = "https://api.exchangeratesapi.io/latest?symbols=" + chosenCurrencyOne + "," + chosenCurrencyTwo;
+
+  $.getJSON( url , function(data) {
+    console.log("Currency Data");
+    console.log(data);
+  });
 });
