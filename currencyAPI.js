7fe108b7c84f3d92493ffa751330a1e39d7cee99
Lee Robbie <leerobb198@lees-mbp.home>
2020-02-13 00:38:52 +0000

    Shows all currencies in text area dependent on currency chosen.

--- currencyAPI.js
+++ currencyAPI.js
@@ -10,53 +10,59 @@ $("#convertButton").click(function(){
   var ccOne = document.getElementById("currencyOne");
   var chosenCurrencyOne = ccOne.options[ccOne.selectedIndex].value;
 
-  console.log(chosenCurrencyOne);
+  // Get amount from text field
+  var amount = document.getElementById("amount").value;
 
-  var ccTwo = document.getElementById("currencyTwo");
-  var chosenCurrencyTwo = ccTwo.options[ccTwo.selectedIndex].value;
+  console.log(chosenCurrencyOne);
+  console.log(amount);
 
   // URL---------------------------------------
 
-  var url = "https://api.exchangeratesapi.io/latest?base="+ chosenCurrencyOne; //+ "," + chosenCurrencyTwo;
-
-  // var url = "https://api.exchangeratesapi.io/history?start_at=2015-01-01&end_at=2015-02-01"
+  var url = "https://api.exchangeratesapi.io/latest?base="+ chosenCurrencyOne;
 
   $.getJSON( url , function(data) {
 
-    // var euro = 0;
-    // console.log("Currency Data");
-    // console.log(data.rates.GBP);
-    //
-    // euro = 1 / (data.rates.GBP);
-
-    // console.log("Euro rate: " + euro);
-
-    console.log(data);
-
-    console.log(data.rates);
-
     ratesArray = data.rates;
-
-    console.log(ratesArray);
 
     currencyArray = ["GBP", "EUR", "USD", "CAD", "HKD", "ISK", "PHP", "DKK"
                     , "HUF", "CZK", "AUD", "RON", "SEK", "IDR", "INR", "BRL"
                     , "RUB", "HRK", "JPY", "THB", "CHF", "SGD", "PLN", "BGN"
                     , "TRY", "CNY", "NOK", "ZAR", "MXN", "ILS", "KRW", "MYR"]
 
+    fullNameCurrency = {"GBP": "Pound Sterling", "EUR": "Euro"
+                    , "USD": "United States Dollar", "CAD": "Canadian Dollar"
+                    , "HKD": "Hong Kong Dollar", "ISK": "Icelandic Krona"
+                    , "PHP": "Philippine Peso", "DKK": "Danish Krone"
+                    , "HUF": "Hungarian Forin", "CZK": "Czech Koruna"
+                    , "AUD": "Australian Dollar", "RON": "Romanian Leu"
+                    , "SEK": "Swedish Krona", "IDR": "Indonesian Rupiah"
+                    , "INR": "Indian Rupee", "BRL": "Brazilian Real"
+                    , "RUB": "Russian Ruble", "HRK": "Croatian Kuna"
+                    , "JPY": "Japanese Yen", "THB": "Thai Baht"
+                    , "CHF": "Swiss Franc", "SGD": "Singapore Dollar"
+                    , "PLN": "Poland Złoty", "BGN": "Bulgarian Lev"
+                    , "TRY": "Turkish Lira", "CNY": "Chinese Yuan"
+                    , "NOK": "Norwegian Krone", "ZAR": "New Zealand Dollar"
+                    , "MXN": "Mexican Peso", "ILS": "Israeli New Shekel"
+                    , "KRW": "South Korean Won", "MYR": "Malaysian Ringgit"};
+
     // Initial conversion message on text area
-    document.getElementById("currencyResultsTextArea").value += ("1 GBP converts to: \n");
+    document.getElementById("currencyComparisonMessage").innerHTML = ("1 " + chosenCurrencyOne + " converts to: \n");
+    // With amount
+    // document.getElementById("currencyComparisonMessage").innerHTML = (amount + " " + chosenCurrencyOne + " converts to: \n");
 
     for (var i = 0; i < currencyArray.length; i++) {
       if (currencyArray[i] !== chosenCurrencyOne) {
         var fullRate = ratesArray[currencyArray[i]];
 
+        // With amount
+        // var fullRate = amount * (ratesArray[currencyArray[i]]);
+
         rounded2DecRate = Math.round(fullRate * 100) / 100;
 
-        document.getElementById("currencyResultsTextArea").value += (rounded2DecRate + " " + currencyArray[i] + "\n");
+        document.getElementById("currencyResultsTextArea").value += (rounded2DecRate + " " + fullNameCurrency[currencyArray[i]] + " ("+ currencyArray[i] + ")\n");
       }
     }
-    // document.getElementById("currencyResultsTextArea").value += (data.rates.GBP + " Pound Sterling = ");
 
   });
 });
