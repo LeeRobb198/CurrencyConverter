22fe2ff84dcbd4b97df2f9da412e6ae9273f4cd6
Lee Robbie <leerobb198@lees-mbp.home>
2020-02-14 21:08:40 +0000

    Template canvasJS graph

--- graph.js
+++ graph.js
@@ -2,76 +2,36 @@
 
 $("#graphButton").click(function(){
 
-  var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
+  console.log("Click registered");
 
-// -----------------------------------
-
-  // console.log("Click registered");
-  //
-  // var chart = new CanvasJS.Chart("chartContainer", {
-  // 	animationEnabled: true,
-  // 	theme: "light2",
-  // 	title:{
-  // 		text: "Currency Rates"
-  // 	},
-  // 	axisY:{
-  // 		includeZero: false
-  // 	},
-  // 	data: [{
-  // 		type: "line",
-  // 		dataPoints: [
-  // 			{ y: 450 },
-  // 			{ y: 414},
-  // 			{ y: 520, indexLabel: "highest",markerColor: "red", markerType: "triangle" },
-  // 			{ y: 460 },
-  // 			{ y: 450 },
-  // 			{ y: 500 },
-  // 			{ y: 480 },
-  // 			{ y: 480 },
-  // 			{ y: 410 , indexLabel: "lowest",markerColor: "DarkSlateGrey", markerType: "cross" },
-  // 			{ y: 500 },
-  // 			{ y: 480 },
-  // 			{ y: 510 }
-  // 		]
-  // 	}]
-  // });
-  // chart.render();
+  var chart = new CanvasJS.Chart("chartContainer", {
+  	animationEnabled: true,
+  	theme: "light2",
+  	title:{
+  		text: "Currency Rates"
+  	},
+  	axisY:{
+  		includeZero: false
+  	},
+  	data: [{
+  		type: "line",
+  		dataPoints: [
+  			{ y: 450 },
+  			{ y: 414 },
+  			{ y: 520 },
+  			{ y: 460 },
+  			{ y: 450 },
+  			{ y: 500 },
+  			{ y: 480 },
+  			{ y: 480 },
+  			{ y: 410 },
+  			{ y: 500 },
+  			{ y: 480 },
+  			{ y: 510 }
+  		]
+  	}]
+  });
+  chart.render();
 
 });
 
