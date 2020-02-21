a53dd1f7b74f10ce40169cfbbe9d1538f928591a
Lee Robbie <leerobb198@lees-mbp.home>
2020-02-21 22:22:38 +0000

    Trail with hello world

--- server.js
+++ server.js
@@ -4,9 +4,13 @@ const app = express()
 app.use(express.static(__dirname)); // Get the html, js, css and image files
 
 // Send the index page
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + "/index.html");
-})
+// app.get('/', function (req, res) {
+//   res.sendFile(__dirname + "/index.html");
+// })
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
 
 // Listen to the server on port 80
 app.listen(80, function () {
